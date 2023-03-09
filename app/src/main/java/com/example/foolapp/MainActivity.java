ea946bfff79958ed2065f081c1be010405647239
homehome1605 <127390593+dinhtrung1605@users.noreply.github.com>
2023-03-09 17:52:17 +0700

    Login-Sign

--- app/src/main/java/com/example/foolapp/MainActivity.java
+++ app/src/main/java/com/example/foolapp/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.foolapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button Sign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Sign = findViewById(R.id.Sign);
+
+        Sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,SignIn.class));
+            }
+        });
     }
 }
