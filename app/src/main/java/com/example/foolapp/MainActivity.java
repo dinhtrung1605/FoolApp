b9345e24db62fbc080e87b42ba55088b8c912f55
homehome1605 <127390593+dinhtrung1605@users.noreply.github.com>
2023-03-09 18:39:16 +0700

    up1

--- app/src/main/java/com/example/foolapp/MainActivity.java
+++ app/src/main/java/com/example/foolapp/MainActivity.java
@@ -6,22 +6,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button Sign;
+    Button Sign, btnLogin;
+    TextView txtUser, txtPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Sign = findViewById(R.id.Sign);
+        txtUser = findViewById(R.id.txtUser);
+        txtPass = findViewById(R.id.txtPass);
 
+
+        Sign = findViewById(R.id.Sign);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = txtUser.getText().toString();
+                String pass = txtPass.getText().toString();
+
+                if (user.equals("admin") && pass.equals("123")) {
+                    Intent intent = new Intent(MainActivity.this, SignIn.class);
+                    startActivity(intent);
+                    Toast.makeText(MainActivity.this, "Wellcome Login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         Sign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,SignIn.class));
+                startActivity(new Intent(MainActivity.this, SignIn.class));
             }
         });
     }
+
+
 }
--- app/src/main/java/com/example/foolapp/SignIn.java
+++ app/src/main/java/com/example/foolapp/SignIn.java
@@ -8,11 +8,20 @@ import android.view.View;
 import android.widget.Button;
 
 public class SignIn extends AppCompatActivity {
+    Button Sign;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+        Sign = findViewById(R.id.Sign);
 
+        Sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignIn.this,MainActivity.class));
+            }
+        });
     }
 }
